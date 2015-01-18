088fcdc7600dc5e6da9c6c31c5e60375b6b678a2
ichigotake <ichigotake.san@gmail.com>
2015-01-19 01:18:05 +0900

    Fix typo

--- sandbox/lib/where.c
+++ sandbox/lib/where.c
@@ -2,7 +2,7 @@
 #include <where.h>
 #include <utils.h>
 
-static void _sandbox_sqlite_where_and(sandbox_sqlite_where_t *where, char *clause, char* logicalOperator, char* argv)
+static void _sandbox_sqlite_where_add(sandbox_sqlite_where_t *where, char *clause, char *logicalOperator, char *argv)
 {
     if (sandbox_utils_array_size_s(where->conditions) > 0) {
         sandbox_utils_array_push_s(where->logicalOperators, logicalOperator);
@@ -23,41 +23,53 @@ sandbox_sqlite_where_t sandbox_sqlite_where_init()
 
 void sandbox_sqlite_where_and(sandbox_sqlite_where_t *where, char* clause)
 {
-    _sandbox_sqlite_where_and(where, clause, "AND", NULL);
+    _sandbox_sqlite_where_add(where, clause, "AND", NULL);
 }
 
 void sandbox_sqlite_where_and_s(sandbox_sqlite_where_t *where, char* clause, char* argv)
 {
-    _sandbox_sqlite_where_and(where, clause, "AND", argv);
+    _sandbox_sqlite_where_add(where, clause, "AND", argv);
 }
 
 void sandbox_sqlite_where_and_i(sandbox_sqlite_where_t *where, char* clause, int argv)
 {
-    _sandbox_sqlite_where_and(where, clause, "AND", sandbox_utils_i_to_s(argv));
+    _sandbox_sqlite_where_add(where, clause, "AND", sandbox_utils_i_to_s(argv));
 }
 
 void sandbox_sqlite_where_or(sandbox_sqlite_where_t *where, char* clause)
 {
-    _sandbox_sqlite_where_and(where, clause, "OR", NULL);
+    _sandbox_sqlite_where_add(where, clause, "OR", NULL);
 }
 
 void sandbox_sqlite_where_or_s(sandbox_sqlite_where_t *where, char* clause, char* argv)
 {
-    _sandbox_sqlite_where_and(where, clause, "OR", argv);
+    _sandbox_sqlite_where_add(where, clause, "OR", argv);
 }
 
 void sandbox_sqlite_where_or_i(sandbox_sqlite_where_t *where, char* clause, int argv)
 {
-    _sandbox_sqlite_where_and(where, clause, "OR", sandbox_utils_i_to_s(argv));
+    _sandbox_sqlite_where_add(where, clause, "OR", sandbox_utils_i_to_s(argv));
 }
 
 char* sandbox_sqlite_where_get_query(sandbox_sqlite_where_t *where)
 {
-    return "TBD";
+    if (sandbox_sqlite_where_is_empty(where)) {
+        return "";
+    }
+
+    char* query = "";
+//    int length = sandbox_utils_array_size_s(where->conditions);
+//    for (int i=0; i<length; i++) {
+//        if (i > 0) {
+//            sprintf(query, "%s %s", query, where->logicalOperators[i-1]);
+//        }
+//        sprintf(query, "%s (%s)", query, where->conditions[i]);
+//    }
+    return query;
 }
 
 int sandbox_sqlite_where_is_empty(sandbox_sqlite_where_t *where)
 {
-   return sandbox_utils_array_is_empty_s(where->conditions);
+    return sandbox_utils_array_is_empty_s(where->conditions);
 }
 
