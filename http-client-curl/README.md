0ac3fb986447b51011f9df5c6bc5332d8932f0a4
ichigotake <ichigotake.san@gmail.com>
2015-01-23 00:35:01 +0900

    Add http-client-curl

---
+++ http-client-curl/README.md
@@ -0,0 +1,20 @@
+# http-client-curl
+
+## Build
+
+``` sh
+mkdir build
+cd build && cmake .. && make
+```
+
+## Develop Environment
+
+- CLion - CL-140.1740.3
+- cmake - 3.0.0
+- depends on `libcurl`
+
+## Resources
+
+- [kazuho/picojson](https://github.com/kazuho/picojson)
+- [Kazuho@Cybozu Labs: 今更 C++ で JSON パーサ「picojson」を書いたわけ](http://developer.cybozu.co.jp/archives/kazuho/2009/07/c-json-picojson.html)
+- [Big Sky :: ヘッダファイルだけでC++から使えるJSONパーサ「picojson」が凄い！](http://mattn.kaoriya.net/software/lang/c/20090702153947.htm)
---
+++ http-client-curl/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <curl/curl.h>
+#include "picojson.h"
+
+#define MAX_BUF 65535
+
+using namespace std;
+
+static char wr_buf[MAX_BUF];
+static int  wr_index = 0;
+
+size_t handle_response(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    size_t segsize = size * nmemb;
+
+    if ((wr_index + segsize) > MAX_BUF) {
+        *(int *)userp = 1;
+        return 0;
+    }
+
+    memcpy((void *)&wr_buf[wr_index], buffer, (size_t)segsize);
+
+    wr_index += segsize;
+
+    wr_buf[wr_index] = '\0';
+    return segsize;
+}
+
+int main() {
+    CURL *curl;
+    CURLcode ret;
+    char error[256];
+
+    curl = curl_easy_init();
+    if (curl == NULL) {
+        fprintf(stderr, "curl_easy_init");
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, "http://time.jsontest.com/");
+    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_response);
+
+    ret = curl_easy_perform(curl);
+    if (ret != CURLE_OK) {
+        wcout << error << endl;
+    } else {
+        picojson::value v;
+        std::string err;
+        char* json = wr_buf;
+        picojson::parse(v, json, json + strlen(json), &err);
+        picojson::object& o = v.get<picojson::object>();
+        cout << "time: " << o["time"] << endl;
+    }
+
+    curl_easy_cleanup(curl);
+
+    return ret;
+}
